58adf8634086f3ecebfae9df3242ee100cbf7d7f
carneiro <7alncc7@gmail.com>
2020-12-25 21:07:57 -0300

    #4 - Estrutura Condicional 90%

---
+++ Udemy/C# primeiros passos Lógica de Programação e Algoritmos/#4 - Estrutura Condicional/ExercícioUri-1049/ExercícioUri-1049/ExercícioUri-1049/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercícioUri_1049
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String[] tipo = { "vertebrado", "invertebrado" };
+
+            String[] tipo2 = { "ave", "mamifero", "inseto", "anelideo" };
+
+            String[] tipo3 = { "carnivoro", "onivoro", "onivoro", "herbivoro", "hematofago", "herbivoro", "hematofago", "onivoro"};
+
+            String[] animal = { "aguia", "pomba", "homem", "vaca", "pulga", "lagarta", "sanguessuga", "minhoca" };
+
+            String variavelExemplo = Console.ReadLine();
+            String variavelExemplo2 = Console.ReadLine();
+            String variavelExemplo3 = Console.ReadLine();
+
+            if (variavelExemplo == "vertebrado" && variavelExemplo2 == "ave" && variavelExemplo3 == "carnivoro")
+                Console.WriteLine("aguia");
+            else if (variavelExemplo == "vertebrado" && variavelExemplo2 == "ave" && variavelExemplo3 == "onivoro")
+                Console.WriteLine("pomba");
+            else if (variavelExemplo == "vertebrado" && variavelExemplo2 == "mamifero" && variavelExemplo3 == "onivoro")
+                Console.WriteLine("homem");
+            else if (variavelExemplo == "vertebrado" && variavelExemplo2 == "mamifero" && variavelExemplo3 == "herbivoro")
+                Console.WriteLine("vaca");
+            else if (variavelExemplo == "invertebrado" && variavelExemplo2 == "inseto" && variavelExemplo3 == "hematofago")
+                Console.WriteLine("pulga");
+            else if (variavelExemplo == "invertebrado" && variavelExemplo2 == "inseto" && variavelExemplo3 == "herbivoro")
+                Console.WriteLine("lagarta");
+            else if (variavelExemplo == "invertebrado" && variavelExemplo2 == "anelideo" && variavelExemplo3 == "hematofago")
+                Console.WriteLine("sanguessuga");
+            else if (variavelExemplo == "invertebrado" && variavelExemplo2 == "anelideo" && variavelExemplo3 == "onivoro")
+                Console.WriteLine("minhoca");
+
+
+        }
+    }
+}
---
+++ Udemy/C# primeiros passos Lógica de Programação e Algoritmos/#4 - Estrutura Condicional/ExercícioUri-1051/ExercícioUri-1051/ExercícioUri-1051/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+namespace ExercícioUri_1051
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (salario > 0.0 && salario <= 2000.00)
+                Console.WriteLine("Isento");
+
+            else if (salario >= 2000.01 && salario <= 3000.00)
+            {
+                salario = (salario - 2000)*0.08;
+                Console.WriteLine("R$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            else if (salario >= 3000.01 && salario <= 4500.00)
+            {
+                salario = ((1000.00 * 0.08) + (0.18 * 2.00));
+                Console.WriteLine("R$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            else if (salario > 4500.00)
+            {
+                salario = (salario - 4500) * 0.28 + (1500 * 0.18) + (1000 * 0.08);
+                Console.WriteLine("R$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+        }
+    }
+}
