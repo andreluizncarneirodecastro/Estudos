18e5ac0f5956c0a56e9f73582d786278619e1c5d
carneiro <7alncc7@gmail.com>
2021-03-13 21:04:44 -0300

    1. POO | Alura

---
+++ randomStuffs/r.egga/alura/1. CaixaEletronico/CaixaEletronico/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaEletronico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta contaGuilherme = new Conta();
+            contaGuilherme.Numero = 1;
+            contaGuilherme.Deposita(1500.0);
+
+            Cliente clienteGuilherme = new Cliente();
+            clienteGuilherme.nome = "Guilherme";
+            clienteGuilherme.idade = 18;
+
+            contaGuilherme.Titular = clienteGuilherme;
+
+            bool sacou = contaGuilherme.Saca(300.0);//testando idade
+            if (sacou)
+            {
+                MessageBox.Show("Saldo da Conta do Guilherme após saque: " + contaGuilherme.Saldo);
+            }
+            else
+            {
+                MessageBox.Show("Não foi possível sacar da conta do Guilherme");
+            }
+        }
+    }
+}
